3447fa0302f5c2549539f5c3425b17883b508db0
UC227669 <Rajesh.Pabbu@thomsonreuters.com>
2017-11-16 12:37:23 -0600

    initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-todo',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  todos = [];
+
+  addTodo (todo: string) {
+    this.todos.push(todo);
+    console.log(this.todos)
+  }
+
 }
